3b1a758ac22a7ba1487fc912687d2333cfdf3531
RizqiFarezi <rizqifarezi@gmail.com>
2024-10-29 00:58:20 -0700

    ADD: Action button

--- example-table/src/index.tsx
+++
@@ -1,8 +0,0 @@
-// index.tsx
-import ReactDOM from 'react-dom/client'; // Updated import for React 18
-import Tablet from './components/Tablet';
-import './styles.css';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Tablet />
-);
